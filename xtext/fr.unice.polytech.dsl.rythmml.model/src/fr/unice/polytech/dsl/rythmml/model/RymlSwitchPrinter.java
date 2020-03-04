28c3de8bcab8bdd81053a29b0c9e50eef70df348
Damien Massol <damien.massol@etu.unice.fr>
2020-03-04 17:26:49 +0100

    all model object can be converted to java code

--- xtext/fr.unice.polytech.dsl.rythmml.model/src/fr/unice/polytech/dsl/rythmml/model/RymlSwitchPrinter.java
+++ xtext/fr.unice.polytech.dsl.rythmml.model/src/fr/unice/polytech/dsl/rythmml/model/RymlSwitchPrinter.java
@@ -25,44 +25,76 @@ public class RymlSwitchPrinter extends RythmmlSwitch<String>{
 	@Override
 	public String caseMusic(Music music) {
 		StringBuilder stringBuilder = new StringBuilder();
+		
+		stringBuilder.append("ConcreteMIDIVisitor midiVisitor = new ConcreteMIDIVisitor(); \n\n");
+		
 		for(Note n : music.getNote()) //Declare all notes
 			stringBuilder.append(this.caseNote(n));
 		
 		for(Beat b : music.getBeat()) //Declare all beats
 			stringBuilder.append(this.caseBeat(b));
 		
+		for(Bar b : music.getBar()) //Declare all bar
+			stringBuilder.append(this.caseBar(b));
+		
+		for(Pattern pattern : music.getPattern()) //Declare all pattern
+			stringBuilder.append(this.casePattern(pattern));
+		
 		for(Section s : music.getSection())
 			stringBuilder.append(this.caseSection(s));
 		
 		stringBuilder.append(String.format("Music %s = new MusicBuilder()", music.getName()));
 		stringBuilder.append(String.format(".setAuthor(\"%s\").setTitle(\"%s\")", music.getAuthor(), music.getTitle()));
-		//stringBuilder.append(String.format(".addSection(section1)");
+		
+		for(Section s : music.getSection()) {
+			stringBuilder.append(String.format(".addSection(%s)", s.getName()));
+		}
 		
 		stringBuilder.append(".prepare().build(); \n");
+		
+		stringBuilder.append(String.format("\nMIDIPlayer.createSequence(%d);\n" + 
+				"MIDIPlayer.setTempoBPM(%d);\n" + 
+				"%s.accept(midiVisitor);\n" + 
+				"MIDIPlayer.playSequence();", 
+				music.getResolutionPerSlide(), music.getBpm(), music.getName()));
 		return stringBuilder.toString();
 	}
 
 	@Override
 	public String caseSection(Section section) {
 		StringBuilder stringBuilder = new StringBuilder();
+		
+		stringBuilder.append(String.format("Section %s = new SectionBuilder()", section.getName()));
 		for(Pattern p : section.getPattern())
-			stringBuilder.append(this.casePattern(p));
+			stringBuilder.append(String.format(".addPattern(%s)", p.getName()));
+		
+		stringBuilder.append(".build(); \n");
 		return stringBuilder.toString();
 	}
 
 	@Override
 	public String casePattern(Pattern pattern) {
 		StringBuilder stringBuilder = new StringBuilder();
+		
+		stringBuilder.append(String.format("Pattern %s = new PatternBuilder()", pattern.getName()));
+		
 		for(Bar b : pattern.getBar())
-			stringBuilder.append(this.caseBar(b));
+			stringBuilder.append(String.format(".addBar(%s)", b.getName()));
+		
+		stringBuilder.append(".build(); \n");
 		return stringBuilder.toString();
 	}
 
 	@Override
 	public String caseBar(Bar bar) {
 		StringBuilder stringBuilder = new StringBuilder();
-		for(Beat b : bar.getBeat())
-			stringBuilder.append(this.caseBeat(b));
+		
+		stringBuilder.append(String.format("Bar %s = new BarBuilder()", bar.getName()));
+		
+		for(Beat beat : bar.getBeat()) {
+			stringBuilder.append(String.format(".addBeat(%s)", beat.getName()));
+		}
+		stringBuilder.append(".build(); \n");
 		return stringBuilder.toString();
 	}
 
